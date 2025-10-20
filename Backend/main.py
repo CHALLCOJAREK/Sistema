68ad5cf60ccd43b021b458522ef4aaefc9f37fac
Jarek Angelo Challco Juarez <challcojuarezjarekangelo@gmail.com>
2025-10-20 13:25:31 -0500

    feat: agrega carga automática de variables de entorno (Telegram)

--- Backend/main.py
+++ Backend/main.py
@@ -11,8 +11,14 @@ from Backend.utils import listar_comandos, secciones, buscar_comando, ensure_tem
 from Backend.bot_handler import enviar_comando
 import os, json, datetime, logging
 from mimetypes import guess_type
+TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID")
+TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
+TELEGRAM_SESSION = os.getenv("TELEGRAM_SESSION")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
 
-
+print("✅ Variables cargadas correctamente:")
+print("API_ID:", TELEGRAM_API_ID)
+print("BOT:", BOT_USERNAME)
 # ============================================================
 # ⚙️ CONFIGURACIÓN BASE
 # ============================================================
